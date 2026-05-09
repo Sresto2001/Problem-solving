bc9246c2487f1abd465e5be336ecfa5324085d46
baruasb <baruasresto32@gmail.com>
2026-05-09 21:47:04 +0600

    problem-7

---
+++ problem-7.ts
@@ -0,0 +1,11 @@
+function getIntersection(arr1: number[], arr2: number[]): number[] {
+    const result: number[] = [];
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr2.includes(arr1[i])) {
+            result.push(arr1[i]);
+        }
+    }
+
+    return result;
+}
